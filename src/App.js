fb364b4630392d47142b9f8547b54facf8befdf6
Prattay9 <prattaydas9@gmail.com>
2025-03-08 16:13:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Motion from './components/Motion';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Motion/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Concept.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Concept.css'
+const Concept = () => {
+  return (
+    <div className='concept'>
+        <h1>OUR CONCEPT</h1>
+        <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  )
+}
+
+export default Concept
