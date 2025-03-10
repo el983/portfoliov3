affbca81316923fdef96c8e3b7b4c8681480337d
el983 <elpislearningconsultantsllp88@gmail.com>
2025-03-10 18:01:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Motion from './components/Motion';
+import Card3 from './components/Card3';
+import Card5 from './components/Card5';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Motion/>
+      <Card3/>
+      <Card5/>
+    </div>
+  );
+}
+
+export default App;
