a1c57091752ad4ddf30539eecb1afca3d5f21bf7
el983 <elpislearningconsultantsllp88@gmail.com>
2025-03-18 17:36:51 +0530

    Add files via upload

---
+++ src/components/Home.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useRef } from 'react';
+import Navbar from './Navbar';
+import './Home.css';
+import h1 from '../assets/h1.jpg';
+import jc5 from '../assets/jc5.png'
+import client1 from '../assets/client1.png'
+const Home = () => {
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          imageRef.current.classList.add('animate');
+        } else {
+          imageRef.current.classList.remove('animate');
+        }
+      },
+      { threshold: 0.3 }
+    );
+
+    if (imageRef.current) {
+      observer.observe(imageRef.current);
+    }
+
+    return () => {
+      if (imageRef.current) {
+        observer.unobserve(imageRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <div className='home'>
+      <div className="write">
+        <img src={client1} alt="" />
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, dolor!</h1>
+      </div>
+      <div className="h-image" ref={imageRef}>
+        <img src={jc5} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
