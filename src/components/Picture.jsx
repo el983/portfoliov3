981c4d5c8cc496fbd8a2049176cfdc57a76eb4bb
el983 <elpislearningconsultantsllp88@gmail.com>
2025-04-07 18:09:36 +0530

    Add files via upload

---
+++ src/components/Picture.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import forbes from '../assets/Forbes_Sowaka.jpg'
+import './Picture.css'
+// import video1 from "../"
+const Picture = () => {
+  return (
+    <div className='pic' id='pic123'>
+        <video src="/videos/video_1.mp4" autoPlay muted loop></video>
+    </div>
+  )
+}
+
+export default Picture
