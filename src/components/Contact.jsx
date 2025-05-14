4af0435356e03dc13e002ddd011eeb80b8ef87c9
el983 <elpislearningconsultantsllp88@gmail.com>
2025-05-14 23:48:31 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,81 +1,76 @@
-import React from 'react';
-import './Contact.css';
+import React from "react";
+import "./Contact.css";
+import MagneticButton from "./MagneticButton";
 
-const Contact = () => {
+const ProjectForm = () => {
   return (
-    <div className="contact_us_green" id='contact'>
-      <div className="responsive-container-block big-container">
-        <div className="responsive-container-block containerc">
-          <div className="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-7 wk-ipadp-10 line" id="i69b-2">
-            <form className="form-box">
-              <div className="container-block form-wrapper">
-                <div className="head-textc-box">
-                  <p className="textc-blk contactus-head">
-                    Contact us
-                  </p>
-                  
-                </div>
-                <div className="responsive-container-block">
-                  <div className="responsive-cell-block wk-ipadp-6 wk-tab-12 wk-mobile-12 wk-desk-6">
-                    <p className="textc-blk input-title">
-                      FIRST NAME
-                    </p>
-                    <input className="input" name="FirstName" />
-                  </div>
-                  <div className="responsive-cell-block wk-desk-6 wk-ipadp-6 wk-tab-12 wk-mobile-12">
-                    <p className="textc-blk input-title">
-                      LAST NAME
-                    </p>
-                    <input className="input" name="LastName" />
-                  </div>
-                  <div className="responsive-cell-block wk-desk-6 wk-ipadp-6 wk-tab-12 wk-mobile-12">
-                    <p className="textc-blk input-title">
-                      EMAIL
-                    </p>
-                    <input className="input" name="Email" />
-                  </div>
-                  <div className="responsive-cell-block wk-desk-6 wk-ipadp-6 wk-tab-12 wk-mobile-12">
-                    <p className="textc-blk input-title">
-                      PHONE NUMBER
-                    </p>
-                    <input className="input" name="PhoneNumber" />
-                  </div>
-                  {/* Service Dropdown */}
-                  <div className="responsive-cell-block wk-desk-6 wk-ipadp-6 wk-tab-12 wk-mobile-12">
-                    <p className="textc-blk input-title">
-                      SERVICE
-                    </p>
-                    <select className="input dropdown" name="Service">
-                      <option value="" disabled selected>Select a service</option>
-                      <option value="webDevelopment">Advisory Services</option>
-                      <option value="appDevelopment">Training Programs</option>
-                      <option value="uiUxDesign">Global Collaborations</option>
-                      <option value="seoMarketing">Custom Solutions</option>
-                    </select>
-                  </div>
-                  <div className="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-12 wk-ipadp-12">
-                    <p className="textc-blk input-title">
-                      WHAT DO YOU HAVE IN MIND
-                    </p>
-                    <textarea className="textcinput" placeholder="Please enter query..."></textarea>
-                  </div>
-                </div>
-                <div className="btn-wrapper">
-                  <button type="submit" className="submit-btn">
-                    Submit
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
-          <div className="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-5 wk-ipadp-10" id="ifgi">
-            <div className="container-box">
-              <div className="textc-content">
-                <p className="textc-blk contactus-head">
-                  Contact us
-                </p>                
-              </div>
-              <div className="workik-contact-bigbox">
+    <div className="project-form-container">
+      <div className="form-left">
+        <h1>
+          Let's start a<br />
+          project together
+        </h1>
+
+        <div className="form-group">
+          <label>
+            01
+            <br />
+            <span>What's your name?</span>
+          </label>
+          <input type="text" placeholder="John Doe *" />
+        </div>
+
+        <div className="form-group">
+          <label>
+            02
+            <br />
+            <span>What's your email?</span>
+          </label>
+          <input type="email" placeholder="john@doe.com *" />
+        </div>
+
+        <div className="form-group">
+          <label>
+            03
+            <br />
+            <span>What's your phone number?</span>
+          </label>
+          <input type="text" placeholder="+000000000" />
+        </div>
+
+        <div className="form-group">
+          <label>
+            04
+            <br />
+            <span>What services are you looking for?</span>
+          </label>
+          <select>
+            <option value="advisory">Advisory Services</option>
+            <option value="training">Training Programs</option>
+            <option value="collaborations">Global Collaborations</option>
+            <option value="custom">Custom Solutions</option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label>
+            05
+            <br />
+            <span>Your message</span>
+          </label>
+          <textarea placeholder="Hello Joy, can you help me with ... *" />
+        </div>
+
+        <div className="send-button-container">
+          <MagneticButton />
+        </div>
+      </div>
+
+      <div className="form-right">
+        <img src="./images/p6.png" alt="profile" className="profile-img" />
+        <div className="arrow">➔</div>
+
+        <div className="workik-contact-bigbox">
                 <div className="workik-contact-box">
                   <div className="phone textc-box">
                     <img 
@@ -123,12 +118,9 @@ const Contact = () => {
                   </a>
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
 };
 
-export default Contact;
+export default ProjectForm;
